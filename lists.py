254fc3ecf31d80d26e74642b1f52df9392559ccd
Wen-Qian-Zheng <158204243+Wen-Qian-Zheng@users.noreply.github.com>
2024-02-05 12:37:32 -0500

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,58 @@
+# animals = ["Zebra", "Camel", "Ape"]
+# stars count at 0, reference each element with []
+# print(animals[0])
+# for animal in animals:
+#     print(animal)
+
+# for animal in animals:
+#     if(animal == "Camel"):
+#         print("We're in the desert")
+
+# # Strings operate like lists
+# x = "Hello Freshman, you all smell"
+# print(x[0])
+
+# So when you do that ^ since the count starts at 0, it takes the first letter, H
+
+# y = x.upper()
+# print(y)
+
+# ^ NOW THEY'RE ALL IN CAPS WOAH
+
+# words_list = x.split(",")
+# print(words_list)
+
+# So here, it found that comma and split the string based on where the comma is
+
+# words_list = x.split(" ")
+# print(words_list)
+
+# ^ In this way, we can find how many spaces there are and it would separate
+# Das cool
+
+# words_list = x.split()
+# print(len(words_list))
+
+# So if you do that you find how many words there are in a sentence
+
+# THE END
+
+# temp = 75
+# if temp > 68:
+#     print('warm')
+# elif temp == 68:
+#     print('perfect')
+# else:
+#     print('cold')
+
+
+
+# Let's create a function that determines if a number is odd or even
+number = 5
+if number%2 == 0:
+    print('Even')
+else:
+    print('Odd')
+
+
+# Let's create a function to accept a "bill" value and offer a tip of 0%, 15%, 20% or 25% depending on if the service was "bad, okay, good , or great ".
